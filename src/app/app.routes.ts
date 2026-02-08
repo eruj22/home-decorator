ca3d9d77595b3fd921a61197df2f385bb281b682
eruj22 <prci.prnaver2@gmail.com>
2026-02-08 10:04:00 +0100

    initial commit - create basic nav and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'visualizer',
+    loadComponent: () =>
+      import('./features/visualizer/visualizer.component').then(
+        (m) => m.VisualizerComponent,
+      ),
+  },
+];
---
+++ src/app/features/visualizer/visualizer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizer',
+  imports: [],
+  templateUrl: './visualizer.component.html',
+  styleUrl: './visualizer.component.scss'
+})
+export class VisualizerComponent {
+
+}
