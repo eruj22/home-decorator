7de48b4645e1dc993a9276baeac62752195e8131
eruj22 <prci.prnaver2@gmail.com>
2026-02-22 13:11:36 +0100

    setup environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  backendEndpoint: 'https://home-decorator-backend.prci-prnaver2.workers.dev',
+};
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  backendEndpoint: 'http://localhost:8787',
+};
