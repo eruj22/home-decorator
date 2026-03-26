56dc5c9a53f80cd11143f9b037681a84d0d3f213
eruj22 <prci.prnaver2@gmail.com>
2026-03-26 18:32:33 +0100

    move navbar one folder up

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AuthService } from '../../../../core/auth.service';
+import { AuthService } from '../../../core/auth.service';
 
 @Component({
   selector: 'app-navbar',
